1e76bad2e8611693e7b6982dc23e3aeb7e80edd9
DamTerrion <damterrion@yandex.ru>
2014-01-20 21:20:50 +0300

    Line() has reload and has got .pat()-method

--- Simple Line class.py
+++ Simple Line class.py
@@ -1,22 +1,55 @@
-class Line(object):
-    def __init__ (self, x, y, a, v, width, height):
-        self.x = x
-        self.y = y
-        self.a = a
-        self.v = v
-        self.angle = a + v*90
-        self.width = width
-        self.height = height
-
-    def points (self):
-        if v % 2 :
-            h_length = self.height/2.0
-            l_width = self.width
-        else:
-            h_length = self.width/2.0
-            l_width = self.heigth
-        x1 = self.x + h_length * cos(pi*self.angle/180)
-        y1 = self.y + h_length * sin(pi*self.angle/180)
-        x2 = self.x - h_length * cos(pi*self.angle/180)
-        y2 = self.y - h_length * sin(pi*self.angle/180)
-        return (l_width, {'x': x1, 'y': y1}, {'x': x2, 'y': y2})
+class line (object):
+	def __init__ (self, x, y, lenght, width, angle):
+		self.x = x
+		self.y = y
+		self.l = lenght
+		self.w = width
+		self.a = angle
+	
+	def pat (size, reverse):
+		'''
+		if not reverse.type == bool:
+			return 'Error:ReverseType'
+			break
+		if not size in (76, 102, 127, 153):
+			return 'Error:WrongSize'
+			break
+		if self.x > size :
+			return 'Error:TooLargeX'
+			break
+		if self.y > size :
+			return 'Error:TooLargeY'
+			break
+		if self.x < 0 :
+			return 'Error:NegativeX'
+			break
+		if self.y < 0 :
+			return 'Error:NegativeY'
+			break
+		if self.lenght <= 0 :
+			return 'Error:ZeroLenght'
+			break
+		if self.width <= 0 :
+			return 'Error:ZeroWidth'
+			break
+		'''
+		if reverse :
+			x = round(self.x*1000)
+			a = round((self.a % 90)*10)
+		else:
+			x = size - self.x
+			if a :
+				a = round(900 - (self.a % 90)*10)
+			else :
+				a = 0
+		y = round(self.y*1000)
+		
+		if self.a % 180 >= 90 :
+			h = round(self.w*1000)
+			w = round(self.l*1000)
+		else:
+			h = round(self.l*1000)
+			w = round(self.w*1000)
+		
+		return {'x':x, 'y':y, 'h':h, 'w':w, 'a':a, 'str':'X'+str(x)+'Y'+str(y)+'H'+str(h)+'W'+str(w)+'A'+str(a)+';\n'}
+		
