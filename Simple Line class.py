fb2b7e852bdc3de50fc3393926696c364b931b55
DamTerrion <damterrion@yandex.ru>
2014-01-21 11:07:33 +0300

    Update Simple Line class.py

--- Simple Line class.py
+++ Simple Line class.py
@@ -1,55 +1,56 @@
 class line (object):
-	def __init__ (self, x, y, lenght, width, angle):
-		self.x = x
-		self.y = y
-		self.l = lenght
-		self.w = width
-		self.a = angle
-	
-	def pat (size, reverse):
-		'''
-		if not reverse.type == bool:
-			return 'Error:ReverseType'
-			break
-		if not size in (76, 102, 127, 153):
-			return 'Error:WrongSize'
-			break
-		if self.x > size :
-			return 'Error:TooLargeX'
-			break
-		if self.y > size :
-			return 'Error:TooLargeY'
-			break
-		if self.x < 0 :
-			return 'Error:NegativeX'
-			break
-		if self.y < 0 :
-			return 'Error:NegativeY'
-			break
-		if self.lenght <= 0 :
-			return 'Error:ZeroLenght'
-			break
-		if self.width <= 0 :
-			return 'Error:ZeroWidth'
-			break
-		'''
-		if reverse :
-			x = round(self.x*1000)
-			a = round((self.a % 90)*10)
-		else:
-			x = size - self.x
-			if a :
-				a = round(900 - (self.a % 90)*10)
-			else :
-				a = 0
-		y = round(self.y*1000)
-		
-		if self.a % 180 >= 90 :
-			h = round(self.w*1000)
-			w = round(self.l*1000)
-		else:
-			h = round(self.l*1000)
-			w = round(self.w*1000)
-		
-		return {'x':x, 'y':y, 'h':h, 'w':w, 'a':a, 'str':'X'+str(x)+'Y'+str(y)+'H'+str(h)+'W'+str(w)+'A'+str(a)+';\n'}
-		
+    def __init__ (self, x, y, lenght, width, angle):
+        self.x = x
+        self.y = y
+        self.l = lenght
+        self.w = width
+        self.a = angle
+    
+    def pat (size, reverse):
+        
+        if not reverse.type == bool:
+            return {'out':'', 'Error':'ReverseType'}
+            break
+        if not size in (76, 102, 127, 153):
+            return {'out':'', 'Error':'Wrong Size'}
+            break
+        if self.x > size :
+            return {'out':'', 'Error':'Too Large X'}
+            break
+        if self.y > size :
+            return {'out':'', 'Error':'Too Large Y'}
+            break
+        if self.x < 0 :
+            return {'out':'', 'Error':'Negative X'}
+            break
+        if self.y < 0 :
+            return {'out':'', 'Error':'Negative Y'}
+            break
+        if self.lenght <= 0 :
+            return {'out':'', 'Error':'Zero Lenght'}
+            break
+        if self.width <= 0 :
+            return {'out':'', 'Error':'ZeroWidth'}
+            break
+        
+        if reverse :
+            x = round(self.x)
+            a = round(self.a % 900)
+        else:
+            x = size*1000 - self.x
+            if a :
+                a = round(- self.a % 900)
+            else :
+                a = 0
+        y = round(self.y)
+        
+        if self.a % 180 >= 90 :
+            h = round(self.w*1000)
+            w = round(self.l*1000)
+        else:
+            h = round(self.l*1000)
+            w = round(self.w*1000)
+                    
+        out = 'X'+str(x)+'Y'+str(y)+'H'+str(h)+'W'+str(w)+'A'+str(a)+';\n'
+        return {'x':x, 'y':y, 'h':h, 'w':w, 'a':a, 'out':out, 'Error':False}
+    
