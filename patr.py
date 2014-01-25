a95edee216c03d775a1a59ea5b4b3b57ff72c19b
DamTerrion <damterrion@yandex.ru>
2014-01-25 22:20:08 +0300

    Update patr.py

--- patr.py
+++ patr.py
@@ -5,13 +5,15 @@ from math import sin
 class Line (object):
     def __init__ (self, x, y, lenght, width, angle, layer):
         self.x = x
+        self.rx = round(x)
         self.y = y
+        self.ry = round(y)
         self.l = lenght
         self.ls = str(lenght/1000)
         self.w = width
         self.ws = str(width/1000)
         self.a = angle
-        self.rad = (angle * pi() / 1800) % (2 * pi())
+        self.rad = (angle * pi / 1800) % (2 * pi)
         self.layer = layer
         self.cos = cos(self.rad)
         self.sin = sin(self.rad)
@@ -23,18 +25,28 @@ def patr (line, size):
     stack1 = ()
     d = { 'c': size * line.cos, 's': size * line.sin }
     while line.l > size :
-        new_line = Line (line.x + line.l*line.c - d['c']/2, line.y + line.l*line.s - d['s'], size, line.w, line.a, line.layer)
-        line = Line (line.x - d['c']/2, line.y - d['s'], line.l - size, line.w, line.a, line.layer)
+        new_line = Line (line.x + line.l*line.c - d['c']/2,
+                         line.y + line.l*line.s - d['s'],
+                         size, line.w, line.a, line.layer)
+        line = Line (line.x - d['c']/2,
+                     line.y - d['s'],
+                     line.l - size, line.w, line.a, line.layer)
         stack1.append(new_line)
     stack1.append(line)
     stack3 = ()
     for s_line in stack1 :
         stack2 = ()
         while s_line.w > size :
-            new_line = Line (s_line.x + s_line.w*s_line.s - d['s']/2, s_line.y + s_line.w*_line.c - d['c'], s_line.l, size, s_line.a, s_line.layer)
-            s_line = Line (s_line.x - d['s']/2, s_line.y - d['c']/2, s_line.l, s_line.w - size, s_line.a, s_line.layer)
+            new_line = Line (s_line.x + s_line.w*s_line.s - d['s']/2,
+                             s_line.y + s_line.w*_line.c - d['c'],
+                             s_line.l, size, s_line.a, s_line.layer)
+            s_line = Line (s_line.x - d['s']/2,
+                           s_line.y - d['c']/2,
+                           s_line.l,
+                           s_line.w - size,
+                           s_line.a, s_line.layer)
             stack2.append(new_line)
         stack2.append(s_line)
         for item in stack2 :
             stack3.append(item)
-	return stack3
+    return stack3
