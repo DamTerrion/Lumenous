7d76641c1ff308ad928c47bda2b33a0440d4243c
DamTerrion <damterrion@yandex.ru>
2014-01-29 11:39:20 +0300

    Update Line.__init__(self).py

--- Line.__init__(self).py
+++ Line.__init__(self).py
@@ -5,15 +5,23 @@ from math import sin
 class Line (object):
     def __init__ (self, x, y, lenght, width, angle, layer):
         self.x = x
-        self.rx = round(x)
+        self.xr = round(x)
+        
         self.y = y
-        self.ry = round(y)
+        self.yr = round(y)
+        
         self.l = lenght
+        self.lr = round(lenght)
         self.ls = str(lenght/1000)
+        
         self.w = width
+        self.wr = round(width)
         self.ws = str(width/1000)
+        
         self.a = angle
+        self.ar = round(angle)
         self.rad = (angle * pi / 1800) % (2 * pi)
-        self.layer = layer
+        
+        self.layer = str(layer)
         self.cos = cos(self.rad)
         self.sin = sin(self.rad)
