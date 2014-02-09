8f5425dc6b69ecf1991433119bd130a7fe90e4bc
DamTerrion <damterrion@yandex.ru>
2014-02-09 23:08:13 +0300

    Update Rectangle class.py

--- Rectangle class.py
+++ Rectangle class.py
@@ -1,4 +1,5 @@
 from math import atan
+from math import pi
 
 class Rect(x1, y1, x2, y2, x3, y3, x4, y4):
     def __init__ (self, x1, y1, x2, y2, x3, y3, x4, y4):
@@ -8,3 +9,5 @@ class Rect(x1, y1, x2, y2, x3, y3, x4, y4):
         self.point4 = {'x': x4, 'y': y4}
         self.point0 = {'x': (x1+x2+x3+x4)/4, 'y': (y1+y2+y3+y4)/4}
         self.point = (self.point0, self.point1, self.point2, self.point3, self.point4)
+        self.angle1 = atan((y2-y1)/(x2-x1))
+        self.angle2 = self.angle1+pi/2
