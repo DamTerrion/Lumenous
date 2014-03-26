ff61abe0cb1f4bcfd94b01e8885fed258ade3e23
DamTerrion <damterrion@yandex.ru>
2014-03-26 14:39:34 +0000

    Update and rename bak clearing.py to clearing.py

--- clearing.py
+++ clearing.py
@@ -4,19 +4,19 @@ from os import remove
 from time import time as now
 from time import ctime
 
-def remove_old_bak (days=60, direction='bak'):
+def bak (days=60, direction='bak'):
     log = open (direction+'/deleted.log', 'a')
     date = now() - days * 24 * 60 * 60
-    log.write(ctime(now())+',\t'+days+'\n'+
+    log.write(ctime(now())+',\t'+str(days)+'\n'+
               '------------------------\n')
     for name, dirs, files in walk(direction):
         for file in files:
             last = path.getatime(direction+'/'+file)
-            if last < date and file[:-4]='.bak' :
+            if last < date and file[:-4]=='.bak' :
                 try:
                     remove(direction+'/'+file)
                 except Exception:
-                    log.write('\t'+"wasn't removed:\n'+
+                    log.write('\t'+"wasn't removed:\n"+
                               '\t'+file+'\t'+ctime(last)+'\n')
                 else:
                     log.write('\t'+file+'\t'+ctime(last)+'\n')
