01681c68276e6b70bdaa4f27ea021e677043cdc5
DamTerrion Limarus <damterrion@yandex.ru>
2014-05-11 22:12:58 +0400

    Added DXF.get

--- DXF.py
+++ DXF.py
@@ -15,14 +15,11 @@ def clear_object ():
         'closed': None          # Замкнутость полилинии
         }
 
-def open_file (dxf_name=False):
+def _open_ (dxf_name=False):
     if not dxf_name:
         dxf_name = input ('Введите имя файла: ')
     if exists(dxf_name):
-        answer = input ('Подтверждается '+dxf_name+'? ')
-        if any(answer.lower() in item for item in ('да', 'yes', 'верно')):
-            return open (dxf_name, 'r')
-        else: return False
+        return open (dxf_name, 'r')
     elif exists(dxf_name+'.dxf'):
         answer = input ('Подтверждается '+dxf_name+'.dxf? ')
         if any(answer.lower() in item for item in ('да', 'yes', 'верно')):
@@ -31,13 +28,58 @@ def open_file (dxf_name=False):
         print ('Файл не обнаружен.')
         return False
 
-def read (dxf_name=False):
-    dxf_file = open_file(dxf_name)
+def _read_ (dxf_name=False):
+    dxf_file = _open_(dxf_name)
     while not dxf_file:
-        dxf_file = open_file(False)
+        dxf_file = _open_(False)
     data = get_data(dxf_file)
     dxf_file.close()
     return data
+
+######
+
+def _marker_ (marker):    
+    try:
+        assert len(marker) == 4
+        assert marker.strip().isdigit()
+    except AssertionError:
+        return False
+    else: return True
+
+def _value_ (marker, value):
+    marker = int(marker.strip())
+    try:
+        if marker in range (0, 10):
+            value = str(value.strip())
+        elif marker in range (10, 60):
+            value = float(value.strip())
+        elif marker in range (60, 80):
+            value = int(value.strip())
+    except ValueError:
+        return False
+    else: return value
+
+def _couple_ (A, B):
+    marker = int(A.strip())
+    value = _value_ (A, B)
+    return (marker, value)
+
+######
+
+def get_couple (data, i):  
+    A, B = data[i], data[i+1]
+    try: C = data[i+2]
+    except IndexError: C = '123\n'    
+    if _marker_ (A):
+        if _marker_ (C):
+            if not _value_ (A, B) is False:
+                return _couple_ (A, B)
+            else: return False
+        else:
+            if not _marker_ (B):
+                return _couple_ (A, B)
+            else: return False
+    else: return False    
 
 def get_data (dxf_file):
     data = []
@@ -52,62 +94,31 @@ def get_data (dxf_file):
             else: even = not even
     return stack
 
-def couple (A, B):
-    
-    marker = int(A.strip())
-    value = check_value (A, B)
-    return (marker, value)
-
-def check_marker (marker):
-    
-    try:
-        assert len(marker) == 4
-        assert marker.strip().isdigit()
-    except AssertionError:
+def get_object (data, name=False):
+    stack = []
+    if not name:
+        name = input ('Введите имя объекта для выборки: ')
+    for item in data:
+        if item[0] == name:
+            stack.append(item)
+    if len(stack) == 0:
         return False
-    else: return True
+    if len(stack) == 1:
+        return stack[0]
+    return stack
 
-def check_value (marker, value):
-    marker = int(marker.strip())
-    try:
-        if marker in range (0, 10):
-            value = str(value.strip())
-        elif marker in range (10, 60):
-            value = float(value.strip())
-        elif marker in range (60, 80):
-            value = int(value.strip())
-    except ValueError:
-        return False
-    else: return value
+######
 
-def get_couple (data, i):
-  
-    A, B = data[i], data[i+1]
-    try: C = data[i+2]
-    except IndexError: C = '123\n'
-    
-    if check_marker (A):
-        if check_marker (C):
-            if not check_value (A, B) is False:
-                return couple (A, B)
-            else: return False
-        else:
-            if not check_marker (B):
-                return couple (A, B)
-            else: return False
-    else: return False
-    
-
-def data_list (data, name=False, i=0, break_condition=(0, 'EOF')):
+def structurize (data, name=False, i=0, break_condition=(0, 'EOF')):
     stack = (name, [])
     while i < len(data)-1:
         if ( data[i] == break_condition or
             (data[i][0], 'ANY') == break_condition):
             return stack, i
         elif data[i] == (0, 'SECTION'):
-            result, i = data_list (data, data[i+1][1], i+2, (0, 'ENDSEC'))
+            result, i = structurize (data, data[i+1][1], i+2, (0, 'ENDSEC'))
         elif data[i][0] == 0:
-            result, i = data_list (data, data[i][1], i+1, (0, 'ANY'))
+            result, i = structurize (data, data[i][1], i+1, (0, 'ANY'))
         else:
             if data[i] == (66, 1):
                 break_condition = (0, 'SEQEND')
@@ -116,6 +127,13 @@ def data_list (data, name=False, i=0, break_condition=(0, 'EOF')):
         stack[1].append(result)
     if name: return stack, i+1
     else: return stack[1]
+
+def get (dxf_name=False, objects=False):
+    data = _read_(dxf_name)
+    data = structurize(data)
+    if objects:
+        data = get_object(data, objects)
+    return data
 
 def print_stack (stack, spaces=''):
     result = []
@@ -128,23 +146,15 @@ def print_stack (stack, spaces=''):
             result.append(spaces+str(item)+'\n')
     return result
 
-def get_object (data, name):
-    stack = []
-    for item in data:
-        if item[0] == name:
-            stack.append(item)
-    if len(stack) == 0:
-        return False
-    if len(stack) == 1:
-        return stack[0]
-    return stack
+def do_result_file (result_name=False, data):
+    if not result_name:
+        result_name = input ('Введите имя файла для результата: ')
+    new_file = open (result_name, 'w')
+    new_file.write(''.join(data))
+    new_file.close()
 
 if __name__ == '__main__':
-    readed = read('Stuffs/FQ.dxf')
-    data = data_list(readed)
-    entities = get_object(data, 'ENTITIES')
-    for_print = print_stack(entities)
-    new_file = open ('Stuffs/result.txt', 'w')
-    new_file.write(''.join(for_print))
-    new_file.close()
+    data = get('Stuffs/FQ.dxf', 'ENTITIES')
+    printable = print_stack(data)
+    do_result_file ('Stuffs/result.txt', printable)
     print ('Готово!')
