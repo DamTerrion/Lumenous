fae652b67e9a18efec822d769f77c84dd3a3d51c
DamTerrion <damterrion@yandex.ru>
2014-06-18 15:25:08 +0400

    Redundancy was removed

--- local.py
+++ local.py
@@ -1,21 +1,7 @@
 dictionary = dict.fromkeys(['en', 'ru'], {})
 dictionary['en'] = {
-    'FileName'  : 'Input a file name: ',
-    'Confirm'   : 'Are you confirm ',
-    'NotFound'  : 'File not found.',
-    'ObjectName': 'Input name of object for selection: ',
-    'ResultFile': 'Input name of result file: ',
-    'Done'      : 'Done!',
-    'ErrorSent' : '--Error Sentence!--'
     }
 dictionary['ru'] = {
-    'FileName'  : 'Введите имя файла: ',
-    'Confirm'   : 'Подтверждаете ',
-    'NotFound'  : 'Файл не обнаружен',
-    'ObjectName': 'Введите имя объекта для выборки: ',
-    'ResultFile': 'Введите имя файла для результата: ',
-    'Done'      : 'Готово!'
-    'ErrorSent' : '--Ошибка фразы!--'
     }
 
 def phrase (sentence, language='en'):
