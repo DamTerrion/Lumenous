88998336759750da7d0b0cd0bc11ef6aea8b6f81
DamTerrion <damterrion@yandex.ru>
2014-06-25 15:00:45 +0400

    New localization branch

---
+++ local.py
@@ -0,0 +1,28 @@
+dictionary = dict.fromkeys(['en', 'ru'], {})
+dictionary['en'] = {
+    'FileName'  : 'Input a file name: ',
+    'Confirm'   : 'Are you confirm ',
+    'NotFound'  : 'File not found.',
+    'ObjectName': 'Input name of object for selection: ',
+    'ResultFile': 'Input name of result file: ',
+    'Done'      : 'Done!',
+    'ErrorSent' : '--Error Sentence!--'
+    }
+dictionary['ru'] = {
+    'FileName'  : 'Введите имя файла: ',
+    'Confirm'   : 'Подтверждаете ',
+    'NotFound'  : 'Файл не обнаружен',
+    'ObjectName': 'Введите имя объекта для выборки: ',
+    'ResultFile': 'Введите имя файла для результата: ',
+    'Done'      : 'Готово!',
+    'ErrorSent' : '--Ошибка фразы!--'
+    }
+
+def phrase (sentence, language='en'):
+    if type(language) == str:
+        language = language.lower()
+    if not language in dictionary:
+        return phrase(sentence)
+    if sentence in dictionary[language]:
+        return dictionary[language][sentence]
+    else: return dictionary[language]['ErrorSent']
