ac80340be21359bd84125bcfdb481fb2f0ae82f9
DamTerrion <damterrion@yandex.ru>
2014-08-06 18:03:03 +0400

    PEP-8 & comment translation

--- ndxf.py
+++ ndxf.py
@@ -1,67 +1,93 @@
 from os import replace, path, mkdir
 from time import ctime, time as now
-import clearing
 from local import phrase
+import clearing
 
 lang = 'ru'
 
 def ndxf (dxf_name):
     new = Param = Value = ''
     
-    if dxf_name[-4:] != '.dxf' :
+    if not dxf_name.endswith('.dxf'):
         dxf_name += '.dxf'
     print (dxf_name)
     
     try:
         dxf = open (dxf_name, 'r')
-        # Файл открывается для считывания
+        # File opens for reading
+        ## Файл открывается для считывания
     except FileNotFoundError:
         phrase('File not found', lang)
         return False
     
-    Allowed = { '_INIT_':    (' 0', ' 8'),      # Имя и слой каждого объекта
-                'POLYLINE':  ('40', '41',       # Начальная и конечная ширина линии
-                              '66', '70'),      # Флаг примитивов и замкнутость линии
-                'SOLID':     ('10', '20',       # Координаты первой вершины
-                              '11', '21',       # Координаты второй вершины
-                              '12', '22',       # Координаты третьей вершины
-                              '13', '23'),      # Координаты четвёртой вершины
-                'VERTEX':    ('10', '20',       # Координаты точки
-                              '42'),            # Выпуклость линии
-                'LINE':      ('10', '20',       # Координаты начала отрезка
-                              '11', '21',),     # Координаты конца  отрезка
-                'ARC':       ('10', '20',       # Координаты центра дуги
-                              '40', '50', '51'),# Радиус, начальный и конечный углы дуги
-                'CIRCLE':    ('10', '20',       # Координаты центра
-                              '40'),            # Радиус окружности
-                'SEQEND':    ()}                # Объект окончания полилинии
+    Allowed = { '_INIT_':    (' 0', ' 8'),
+                            # Name & layer for every object
+                            ## Имя и слоя для любого объекта
+                'POLYLINE':  ('40', '41',
+                            # Start & end width for polyline
+                            ## Начальная и конечная ширина полилинии
+                              '66', '70'),
+                            # Flags for primitives & enclosed
+                            ## Флаги примитивов и замкнутости
+                'SOLID':     ('10', '20', '11', '21', '12', '22', '13', '23'),
+                            # Coordinates for four vertexs
+                            ## Координаты четырёх вершин
+                'VERTEX':    ('10', '20', '42'),  
+                            # Single vertex coordinates & сovexity of line
+                            ## Координаты отдельной вершины и выпуклость
+                'LINE':      ('10', '20', '11', '21',),
+                            # Coord-s of line's start & end
+                            ## Координаты начала и конца отрезка
+                'ARC':       ('10', '20',
+                            # Center's coord-s of arc
+                            ## Координаты центра дуги
+                              '40', '50', '51'),
+                            # Radius, starting & ending angles
+                            ## Радиус, начальный и конечный углы дуги
+                'CIRCLE':    ('10', '20', '40'),
+                            # Center's coord-s & radius of circle
+                            ## Координаты центра и радиус окружности
+                'SEQEND':    ()}
+                            # Object "polyline is ends here"
+                            ## Объект завершения полилинии
     
     while not (Param == '  2\n' and Value == 'ENTITIES\n'):
-        # Если не начался раздел ENTITIES, файл просто просматривается
+        # Till 'Entities' section starts file just viewed
+        ## Если не начался раздел ENTITIES, файл просто просматривается
         Param = dxf.readline()
         Value = dxf.readline()
         
     else :
-        # Если же раздел ENTITIES начался, сразу же записывается его заголовок
-        new += '  0\n'+'SECTION\n'+'  2\n'+'ENTITIES\n'
+        # When 'Entites' starts it's write header
+        ## Если же раздел ENTITIES начался, записывается его заголовок
+        new = ''.join('  0\n', 'SECTION\n',
+                      '  2\n', 'ENTITIES\n')
         inObject = ''
         
         while not (Param == '  0\n' and Value == 'ENDSEC\n'):
-            # Цикл работает до тех пор, пока не закончится раздел
+            # Loop working until section will ended
+            ## Цикл работает до тех пор, пока не закончится раздел
             Param = dxf.readline()
             Value = dxf.readline()
             
-            if Param == '  0\n' :       # Если встречается код 0, значит, начался объект,
-                inObject = Value[:-1]   #  и требуется определить и сохранить тип объекта
+            if Param == '  0\n' :
+                inObject = Value[:-1]
+                # If code 0 found, it mean "new object begins";
+                #  it must determine and save type of new object
+                ## Если встретился код 0, значит, начался объект,
+                ##  и требуется определить и сохранить тип объекта
 
             if (inObject in Allowed and
                 (Param[1:-1] in Allowed['_INIT_'] or
                  Param[1:-1] in Allowed[inObject])):
-                    # Если рассматривается правильная пара объект-параметр, она записывается
-                    new += Param + Value
+                    # If it's right couple 'object-parameter', it saving
+                    ## Если рассматривается правильная пара
+                    ##  объект-параметр, она записывается
+                    new = ''.join(Param, Value)
         else:
-            # Раздел кончился, можно расслабиться и завершить запись данных
-            new += '  0\n'+'ENDSEC\n'+'  0\n'+'EOF\n'
+            # Section is ended, data's writing can be completed
+            ## Раздел кончился, можно завершить запись данных
+            new = '  0\n'.join('ENDSEC\n','EOF\n')
             
     dxf.close()
     if path.exists('bak') == False :
@@ -71,36 +97,41 @@ def ndxf (dxf_name):
     except Exception:
         replace (dxf_name, dxf_name+'.bak')
         phrase("Can't replace this file to '/bak'!", lang)
-    #replace (dxf_name, 'bak/'+dxf_name+'.bak')
+    ''' replace (dxf_name, 'bak/'+dxf_name+'.bak') '''
     # Обрабатывавшийся файл <name>.dxf превращается в bak/<name>.dxf.bak
     # Если произошла ошибка (невозможно файл переместить в папку bak/),
     #  то он переименовывается в <name>.dxf.bak там, где и был, с заменой
 
-    # Так как имя исходного файла сменилось, он создаётся заново
+    # As name of otigin file was changed, it recreating
+    ## Так как имя исходного файла сменилось, он создаётся заново
     try:
         dxf = open (dxf_name, 'w')    
         dxf.write(new)
         dxf.close()
-        # Попытка записи в файл с перехватом ошибок
+        # Trying to save to file with exceptions trapping
+        ## Попытка записи в файл с перехватом ошибок
     except Exception:
         log = open ('bak/processed.log', 'a')
-        log.write('\t'+
-                  'Error!'+'\t'+
-                  full_name[1]+'\t|\t'+
-                  full_name[0]+'\t|\t'+
-                  ctime(now())+'\n')
+        log.write('\t'.join('Error',
+                            full_name[1], '|',
+                            full_name[0], '|',
+                            ctime(now()), '\n'
+                            ))
         log.close()
         prase ('Mistake was maked during processing', lang)
-        # Если возникла ошибка, то сообщение об этом выводится в лог
+        # If was found an exception, it's logging to log
+        ## Если возникла ошибка, то сообщение об этом выводится в лог
     else:
         if len(full_name[1]) < 8: t = '\t\t'
         else: t = '\t'
         log = open ('bak/processed.log', 'a')
-        log.write(full_name[1]+t+'|\t'+
-                  full_name[0]+'\t|\t'+
-                  ctime(now())+'\n')
+        log.write(''.join(full_name[1], t,'|\t',
+                          full_name[0], '\t|\t',
+                          ctime(now()), '\n'
+                          ))
         log.close()
         phrase('All done!', lang)
+        # If wasn't found any exceptions, processe's information logging
         # Если не возникло ошибок, в лог выводится информация об обработке
 
 def call_clearing (command=False):
@@ -157,10 +188,11 @@ def confirm (status=None, recursive=False):
         elif status in wrong: return False
         else: return confirm('FaultStr', recursive)
 
+# -- It need to be remaked --
 def need_clear (days=45):
     try:
         count = clearing.bak(days)
-        #print ('Произведена чистка файлов старше', need_clear, 'дней.')
+        ''' print ('Произведена чистка файлов старше', need_clear, 'дней.') '''
         phrase(('All files older than _ days was deleted.', need_clear), lang)
         if (count%10 == 1 and
             count%100 != 11):
@@ -173,22 +205,27 @@ def need_clear (days=45):
     except Exception:
         return False
     else: return True
+# -- It need to be remaked --
 
-__author__ = 'Максим "ДамТеррион" Соловьёв'
+__author__ = 'Maksim "DamTerrion" Solovev'
 
 if __name__ == '__main__':
     need = phrase('Activate clearing? (Yes/Not/Days)', lang, 'input')
     call_clearing(need)
-    # Подчистка мусора на выбор пользователя,
-    #  если этот скрипт запущен как основной,
-    #  а не импортирован из другого скрипта.
+    # Garbage cleaning from user's choice
+    #  if this script is main script
+    ## Подчистка мусора на выбор пользователя,
+    ##  если этот скрипт запущен как основной,
+    ##  а не импортирован из другого скрипта.
  
 while __name__ == '__main__':
-    # Цикл выполняется, пока это главный скрипт,
-    #  а не импортированный
+    # Loop working until this is a main script
+    ## Цикл выполняется, пока это главный скрипт,
+    ##  а не импортированный
     code = phrase('Input name of DXF-file for processing', lang, 'input')
     if code in ('quit', 'exit', 'выход', 'конец') : break
     try: ndxf (code)
     except Exception: phrase('Mistake was making', lang)
-    # Строго говоря, не представляю, как скрипт может
-    #  внутри цикла перестать быть главным
+    # I havn't any idea, how script can be stop being main in loop
+    ## Строго говоря, не представляю, как скрипт может
+    ##  внутри цикла перестать быть главным
