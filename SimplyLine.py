7df63340ef1fd49caccc1a999d3e70bb1a3f554f
DamTerrion <damterrion@yandex.ru>
2014-08-08 17:21:22 +0400

    File "SimplyLine" (class container) positively rewrited

--- SimplyLine.py
+++ SimplyLine.py
@@ -1,14 +1,15 @@
 from math import pi, cos, sin, radians
 
-class Line (object):
+class Exponion (object):
     def __init__ (self,
                   x, y,
-                  lenght,
+                  height,
                   width,
                   angle,
                   layer='0',
                   origin=None,
-                  colour=None):
+                  colour=None
+                  ):
         
         self.x  = int(x)
         self.xr = round(self.x)
@@ -16,9 +17,9 @@ class Line (object):
         self.y  = int(y)
         self.yr = round(self.y)
         
-        self.l  = int(lenght)
-        self.lr = round(self.l)
-        self.ls = str(self.l/1000)
+        self.h  = int(height)
+        self.hr = round(self.h)
+        self.hs = str(self.h/1000)
         
         self.w  = int(width)
         self.wr = round(self.w)
@@ -36,7 +37,7 @@ class Line (object):
         self.origin = str(origin)
         
         self.xy = complex (self.x, self.y)
-        self.lw = complex (self.w, self.l)
+        self.hw = complex (self.w, self.h)
         self.ac = complex (self.cos,
                            self.sin)
     
@@ -49,49 +50,49 @@ class Line (object):
                 '153':  60}
         
         if str(size) in Rang:
-            Allowed = [int(size)/2-Rang(str(size)),
-                       int(size)/2+Rang(str(size))]
-        else: return { 'Correct': 0 }
+            Allowed = (int(size)/2 - Rang(str(size)),
+                       int(size)/2 + Rang(str(size))
+                       )
+        else: return {'Correct': 0}
         
         if (abs(self.xr - self.x) > 0.1 or
-            abs(self.yr - self.y) > 0.1 ):
+            abs(self.yr - self.y) > 0.1
+            ):
             Correct = 2
         
         if (self.x%0.25 > 0.03 or
-            self.y%0.25 > 0.03 ):
+            self.y%0.25 > 0.03
+            ):
             Correct = 3
         
         if (self.x < Allowed[0] or
             self.x > Allowed[1] or
             self.y < Allowed[0] or
-            self.y > Allowed[1] ):
+            self.y > Allowed[1]
+            ):
             Correct += 4
         
         if (self.x < 0 or self.x > int(size) or
-            self.y < 0 or self.y > int(size) ):
+            self.y < 0 or self.y > int(size)
+            ):
             Correct += 4
         
-        return { 'Correct': Correct }
+        return {'Correct': Correct}
     
     def coords (self):
-        va = self.ac * ( self.lw.real )
-        vb = self.ac * ( self.lw - va )
+        va = self.ac * (self.hw.real)
+        vb = self.ac * (self.hw - va)
         
         return {'A': self.xy + va,
                 'B': self.xy + vb,
                 'C': self.xy - va,
                 'D': self.xy - vb,
                 
-                '1': self.xy - va - vb,
-                '2': self.xy + va - vb,
-                '3': self.xy - va + vb,
-                '4': self.xy + va - vb
+                '1': self.xy - va-vb,
+                '2': self.xy + va-vb,
+                '3': self.xy - va+vb,
+                '4': self.xy + va-vb
                 }
-
-    def colour_string (self):
-        if self.colour:
-            return ' 62\n{:>6}\n'.format(self.colour)
-        return ''
     
     def pat (self, size=102, reverse=False):
         
@@ -100,7 +101,7 @@ class Line (object):
         else:
             Check = self.check()
         if not Check['Correct']:
-            return {'out': '', 'Error': Check['Error'] }
+            return {'out': '', 'Error': Check['Error']}
         
         if reverse :
             x = self.xr
@@ -115,156 +116,106 @@ class Line (object):
         
         if self.a % 1800 >= 900 :
             h = self.wr
-            w = self.lr
+            w = self.hr
         else:
-            h = self.lr
+            h = self.hr
             w = self.wr
         
         return {'x': x, 'y': y, 'h': h, 'w': w, 'a': a,
-                'out': ('X'+str(x)+
-                        'Y'+str(y)+
-                        'H'+str(h)+
-                        'W'+str(w)+
-                        'A'+str(a)+';\n') }
+                'out': ''.join(('X', str(x),
+                                'Y', str(y),
+                                'H', str(h),
+                                'W', str(w),
+                                'A', str(a), ';\n'
+                                ))
+                }
     
     def polyline (self):
         coord = self.coords()
-        
-        A = {'x': coord['A'].real,
-             'y': coord['A'].imag }
-        
-        C = {'x': coord['C'].real,
-             'y': coord['C'].imag }
-        
-        return (' 0\nPOLYLINE\n'+
-                ' 8\n'+self.layer+'\n'+
-                colour_string()+
-                ' 66\n     1\n'+
-                ' 40\n'+self.ws+'\n'+
-                ' 41\n'+self.ws+'\n'+
-                ' 0\nVERTEX\n'+
-                ' 8\n'+self.layer+'\n'+
-                ' 10\n'+str(C['x']/1000)+'\n'+
-                ' 20\n'+str(C['y']/1000)+'\n'+
-                ' 30\n0.0\n'+
-                ' 0\nVERTEX\n'+
-                ' 8\n'+self.layer+'\n'+
-                ' 10\n'+str(A['x']/1000)+'\n'+
-                ' 20\n'+str(A['y']/1000)+'\n'+
-                ' 30\n0.0\n'+
-                ' 0\nSEQEND\n'+
-                ' 8\n'+self.layer+'\n' )
+        result = ['  0', 'POLYLINE',
+                  '  8', self.layer]
+        if self.colour:
+            result += [' 62', '{:>6}'.format(self.colour)]
+        result += [' 66', '     1',
+                   ' 40', self.ws,
+                   ' 41', self.ws]
+        result += vertex(coord['C'])
+        result += vertex(coord['A'])
+        result += ['  0', 'SEQEND',
+                   '  8', self.layer]
+        return result
     
     def rectangle (self):
         coord = self.coords()
-        
-        p1 = {'x': coord['1'].real,
-              'y': coord['1'].imag }
-        
-        p2 = {'x': coord['2'].real,
-              'y': coord['2'].imag }
-        
-        p3 = {'x': coord['4'].real,
-              'y': coord['4'].imag }
-        
-        p4 = {'x': coord['3'].real,
-              'y': coord['3'].imag }
-        
-        return ('  0\nPOLYLINE\n'+
-                '  8\n'+self.layer+'\n'+
-                colour_string()+
-                ' 66\n     1\n'+
-                ' 40\n0.0\n'+
-                ' 41\n0.0\n'+
-                ' 70\n1\n'+
-                ' 0\nVERTEX\n'+
-                ' 8\n'+self.layer+'\n'+
-                ' 10\n'+str(p1['x']/1000)+'\n'+
-                ' 20\n'+str(p1['y']/1000)+'\n'+
-                ' 30\n0.0\n'+
-                ' 0\nVERTEX\n'+
-                ' 8\n'+self.layer+'\n'+
-                ' 10\n'+str(p2['x']/1000)+'\n'+
-                ' 20\n'+str(p2['y']/1000)+'\n'+
-                ' 30\n0.0\n'+
-                ' 0\nVERTEX\n'+
-                ' 8\n'+self.layer+'\n'+
-                ' 10\n'+str(p3['x']/1000)+'\n'+
-                ' 20\n'+str(p3['y']/1000)+'\n'+
-                ' 30\n0.0\n'+
-                ' 0\nVERTEX\n'+
-                ' 8\n'+self.layer+'\n'+
-                ' 10\n'+str(p4['x']/1000)+'\n'+
-                ' 20\n'+str(p4['y']/1000)+'\n'+
-                ' 30\n0.0\n'+
-                ' 0\nSEQEND\n'+
-                ' 8\n'+self.layer+'\n' )
+        result = ['  0', 'POLYLINE',
+                  '  8', self.layer]
+        if self.colour:
+            result += [' 62', '{:>6}'.format(self.colour)]
+        result += [' 66', '     1',
+                   ' 40', '0.0',
+                   ' 41', '0.0',
+                   ' 70', '     1']
+        result += vertex(coord['1'])
+        result += vertex(coord['2'])
+        result += vertex(coord['4'])
+        result += vertex(coord['3'])
+        result += ['  0', 'SEQEND'+
+                   '  8', self.layer]
+        return result
     
     def solid (self):
         coord = self.coords()
-        
-        p1 = {'x': coord['1'].real,
-              'y': coord['1'].imag }
-        
-        p2 = {'x': coord['2'].real,
-              'y': coord['2'].imag }
-        
-        p3 = {'x': coord['3'].real,
-              'y': coord['3'].imag }
-        
-        p4 = {'x': coord['4'].real,
-              'y': coord['4'].imag }
+        result = ['  0', 'SOLID',
+                  '  8', self.layer]
+        if self.colour:
+            result += [' 62', '{:>6}'.format(self.colour)]
+        result += vertex(coord['1'], False)
+        result += vertex(coord['2'], False)
+        result += vertex(coord['3'], False)
+        result += vertex(coord['4'], False)
+        return result
 
-        return ('  0\nSOLID\n'+
-                '  8\n'+self.layer+'\n'+
-                colour_string()+
-                ' 10\n'+str(p1['x']/1000)+'\n'+
-                ' 20\n'+str(p1['y']/1000)+'\n'+
-                ' 30\n0.0\n'+
-                ' 11\n'+str(p2['x']/1000)+'\n'+
-                ' 21\n'+str(p2['y']/1000)+'\n'+
-                ' 31\n0.0\n'+
-                ' 12\n'+str(p3['x']/1000)+'\n'+
-                ' 22\n'+str(p3['y']/1000)+'\n'+
-                ' 32\n0.0\n'+
-                ' 13\n'+str(p4['x']/1000)+'\n'+
-                ' 23\n'+str(p4['y']/1000)+'\n'+
-                ' 33\n0.0\n' )
-
-class LStack (list):
+class Poynting (list):
     def __init__ (self, stack=None, data_type=None):
-        if ( data_type.lower() == 'dxf' and
-             type(stack) == list):
+        if (data_type.lower() == 'dxf' and
+            isinstance(stack, list)):
             pass
-        if ( data_type.lower() == 'pat' and
-             type(stack) == list):
+        
+        if (data_type.lower() == 'pat' and
+            isinstance(stack, list)):
+            pass
+        
+        if (data_type.lower() == 'lum' and
+            isinstance(stack, list)):
             pass
     
     def add (self, addition):
-        if type(addition) == Line:
+        if isinstance(addition, Exponion):
             self.append(addition)
-        elif ( type(addition) == LStack or
-               type(addition) == list ):
+        elif isinstance(addition, Poynting) or
+             isinstance(addition, list):
             self.extend(addition)
     
     def extend (self, stack):
         for item in stack:
-            if type(item) == Line:
+            if isinstance(item, Exponion):
                 self.append(item)
     
-    def generate (self, form, mode):
-        if not form in ('pat', 'dxf'): return False
+    def generate (self, form='lum', mode=None):
+        if not (isinstance(form, str) and
+                form.lower() in ('pat', 'dxf', 'lum')
+                ):
+            return False
         
-        result = ''
+        result = []
         if form == 'pat':
             for item in self:
                 result += item.pat(mode[0], mode[1])
-            result += '$;'
-            return result
+            result.append('$;')
         
         if form == 'dxf':
-            result += ('  0\nSECTION\n'+
-                       '  2\nENTITIES\n')
+            result += ['  0', 'SECTION',
+                       '  2', 'ENTITIES']
             for item in self:
                 if mode == 'polyline':
                     result += self.polyline()
@@ -274,41 +225,59 @@ class LStack (list):
                     result += self.rectangle()
                 elif self.orig == 'SOLID':
                     result += self.solid()
-                else: result += self.polyline()
-            result += ('  0\nENDSEC\n'+
-                       '  0\nEOF\n')
-            return result
+                else:
+                    result += self.polyline()
+            result += ['  0', 'ENDSEC',
+                       '  0', 'EOF']
+
+        if form == 'lum':
+            pass
+
+        return '\n'.join(result)
         
     def layer (self, name=False):
         if name:
-            result = LStack()
+            result = Poynting()
             for item in self:
                 if item.layer == name: result.add(item)
             return result
 
         result = dict()
         for item in self:
-            if not item.layer in self:
-                result[item.layer] = LStack()
+            if not item.layer in result:
+                result[item.layer] = Poynting()
             result[item.layer].add(item)
         return result
     
     def data_file (self, name='data'):
-        result = name+' = {\n'
+        result = [name, ' = {\n']
         space = ' '*4
         for item in self:
-            result += (space + 'Line( '+
-                       str(self.x)+', '+
-                       str(self.y)+', '+
-                       str(self.l)+', '+
-                       str(self.w)+', '+
-                       str(self.a)+', '+
-                       self.layer +', '+
-                       self.origin+', '+
-                       self.colour+')\n')
-        result += space + '}\n'
-        return result
+            result += [space,
+                       'Exponion(',
+                       str(self.x), ', ',
+                       str(self.y), ', ',
+                       str(self.h), ', ',
+                       str(self.w), ', ',
+                       str(self.a), ', ',
+                       self.layer,  ', ',
+                       self.origin, ', ',
+                       self.colour, ')\n']
+        result += [space, '}\n']
+        return ''.join(result)
     
     def check (self, size):
         for item in self:
             pass
+
+def vertex (point, full=True):
+    if not isinstance(point, complex): return False
+    result = []
+    if full:
+        result += ['  0', 'VERTEX',
+                   '  8', self.layer]
+    result += [' 10', str(point.real/1000),
+              [' 20', str(point.imag/1000)]
+    if full:
+        result += [' 30', '0.0']
+    return result
