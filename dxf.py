5fbc4904a26a602b5ae4bb8fc5b376d0258c54b5
DamTerrion Limarus <damterrion@yandex.ru>
2014-09-15 00:58:06 +0400

    dxf.py renamed with few correction

--- dxf.py
+++ dxf.py
@@ -1,21 +1,7 @@
-from math import atan2, degrees
 from os.path import exists
-from Exponion import Exponion, Poynting
 from local import say
 
-precision = -4
 lang = 'ru' # Ignore this string, if you want to use english in program
-
-def clear_object ():
-    return {
-        'type'  : None,         # Тип объекта (линия, фигура)
-        'layer' : '0',          # Слой объекта
-        'colour': 'BYLAYER',    # Цвет объекта, если он задан явно
-        'points': [],           # Словарь точек со стороковыми именами
-        'vertex': 0,            # Счётчик точек полилинии - число
-        'width' : None,         # Ширина полилинии
-        'closed': None          # Замкнутость полилинии
-        }
 
 '''
 def _open_ (dxf_name=False):
@@ -36,17 +22,26 @@ def _read_ (dxf_name=False):
     dxf_file = _open_(dxf_name)
     while not dxf_file:
         dxf_file = _open_(False)
-    data = get_data(dxf_file)
+    data = extract(dxf_file)
     dxf_file.close()
     return data
 '''
 
 # Нужно отладить функцию загрузки файла
 # Хорошо бы добавить функцию выгрузки файла
+
+# Нужно убрать функцию 'get', заменив более функциональной
+def get (objects, dxf_name=False):
+    data = _read_(dxf_name)
+    data = structurize(data)
+    if objects:
+        data = elicit(data, objects)
+    return data
+
 def load (dxf_name):
     try:
         dxf_file = open(dxf_name, 'r')
-        data = get_data(dxf_file)
+        data = extract(dxf_file)
     except OSError:
         say('Ошибка открытия файла', lang)
     except Exception:
@@ -54,6 +49,43 @@ def load (dxf_name):
     finally:
         dxf_file.close()
     return data
+
+def extract (dxf_file):
+    data = []
+    for line in dxf_file:
+        data.append(line)
+    stack = []
+    even = True
+    for i in range(0, len(data)-1):
+        if even != i%2:
+            couple = get_couple(data, i)
+            if couple: stack.append(couple)
+            else: even = not even
+    return stack
+
+def structurize (data, name=False, i=0, break_condition=(0, 'EOF')):
+    if name:
+        stack = [name]
+    else:
+        stack = dict()
+    while i < len(data)-1:
+        if (data[i] == break_condition or
+            (data[i][0], 'ANY') == break_condition):
+            return stack, i
+        elif data[i] == (0, 'SECTION'):
+            result, i = structurize (data, data[i+1][1], i+2, (0, 'ENDSEC'))
+        elif data[i][0] == 0:
+            result, i = structurize (data, data[i][1], i+1, (0, 'ANY'))
+        else:
+            if data[i] == (66, 1):
+                break_condition = (0, 'SEQEND')
+        # Тут должны быть инструкции, определяющие добавление
+        #  элементов в звисимости от типа стэка
+            result = data[i]
+            i += 1
+        # stack[1].append(result)
+    if name: return stack, i+1
+    else: return stack[1]
 
 # Нужно разобраться с функцией получения пары
 def get_couple (data, i):
@@ -82,33 +114,13 @@ def get_couple (data, i):
     A, B = data[i], data[i+1]
     try: C = data[i+2]
     except IndexError: C = '123\n'    
-    if _marker_(A):
-        if _marker_(C):
-            if not _value_(A, B) is False:
-                return _couple_(A, B)
-            else: return False
-        else:
-            if not _marker_(B):
-                return _couple_(A, B)
-            else: return False
+    if (_marker_(A) and
+        ((_marker_(C) and _value_(A, B)) or
+         not _marker_(B))
+        ): return _couple_(A, B)
     else: return False
 
-# Нужно переименовать 'get_data' в 'extract'
-def get_data (dxf_file):
-    data = []
-    for line in dxf_file:
-        data.append(line)
-    stack = []
-    even = True
-    for i in range(0, len(data)-1):
-        if even != i%2:
-            couple = get_couple(data, i)
-            if couple: stack.append(couple)
-            else: even = not even
-    return stack
-
-# Нужно переименовать 'get_object' в 'elicit'
-def get_object (data, name=False):
+def elicit (data, name=False):
     stack = []
     if not name:
         name = say('Input name of object for selection: ', lang, 'input')
@@ -123,34 +135,9 @@ def get_object (data, name=False):
 
 ######
 
-def structurize (data, name=False, i=0, break_condition=(0, 'EOF')):
-    stack = (name, [])
-    while i < len(data)-1:
-        if (data[i] == break_condition or
-            (data[i][0], 'ANY') == break_condition):
-            return stack, i
-        elif data[i] == (0, 'SECTION'):
-            result, i = structurize (data, data[i+1][1], i+2, (0, 'ENDSEC'))
-        elif data[i][0] == 0:
-            result, i = structurize (data, data[i][1], i+1, (0, 'ANY'))
-        else:
-            if data[i] == (66, 1):
-                break_condition = (0, 'SEQEND')
-            result = data[i]
-            i += 1
-        stack[1].append(result)
-    if name: return stack, i+1
-    else: return stack[1]
-
-# Нужно убрать функцию 'get', заменив более функциональной
-def get (objects, dxf_name=False):
-    data = _read_(dxf_name)
-    data = structurize(data)
-    if objects:
-        data = get_object(data, objects)
-    return data
-
-# Нужно проверить функциональность функции 'get_head'
+'''
+# Функция 'get_head' возвращает список имён по данным
+# Она нигде не используется
 def get_head (data):
     stack = []
     for item in data:
@@ -159,6 +146,7 @@ def get_head (data):
         if type(item) in (int, str):
             stack.append(item)
     return stack
+'''
 
 # Нужно проверить функциональность функции 'print_stack'
 # Вероятно, эта функция должна быть вынесена вовне
@@ -175,7 +163,7 @@ def print_stack (stack, spaces=''):
 
 # Нужно выяснить, что вообще за чертовщина тут творится!
 if __name__ == '__main__':
-    data = get('ENTITIES')[1]
+    data = load('ENTITIES')[1]
     printable = print_stack(data)
     result_name = say('Input name of result file', lang, 'input')
     new_file = open(result_name, 'w')
